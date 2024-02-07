bc9f6fbe925dd1fb17a1941765f08b8a1956b660
SRams9 <158037041+SRams9@users.noreply.github.com>
2024-02-08 00:47:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Week1 from './components/week1';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>
+      Hello world!
+     </h1>
+     <h2>
+      Name - Sairam
+      Roll no - 04289
+      Group - 7
+     </h2>
+     {/* <p2>
+      Display my component without jsx
+     </p2> */}
+     <hr>
+     </hr>
+     <Week1/>
+    </div>
+    
+  );
+}
+
+export default App;
